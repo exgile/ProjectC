f4118d3fa2867ba93a949c40309e5bf61076d2fc
topkung1 <hyperzaster@gmail.com>
2018-03-01 18:43:04 +0700

    Finish question number 1-4 and add GetDirSize function to get dir tree size

--- Core.pas
+++ Core.pas
@@ -3,16 +3,18 @@ unit Core;
 interface
 
 uses
-  System.Threading, System.SysUtils, System.StrUtils;
+  System.Threading, System.SysUtils, System.Classes, System.StrUtils;
 
 type
   TMainCore = class
     const
       WordFileName: AnsiString = 'words.txt';
+      FolderName: AnsiString = 'Data';
     private
       fCommand: ITask;
       procedure StartCommand;
       procedure StartProcess;
+      function GetDirSize(const Path: String): UInt32;
     public
       constructor Create;
       destructor Destroy;
@@ -26,6 +28,11 @@ constructor TMainCore.Create;
 begin
   // Start the command-line
   Self.StartCommand;
+
+  // First Check whether the main folder exist or not
+  if not DirectoryExists(FolderName) then
+    if not CreateDir(FolderName) then
+      WriteLn(Format('System Error: Directory ''%s'' has not been created', [FolderName]));
 end;
 
 destructor TMainCore.Destroy;
@@ -58,10 +65,14 @@ begin
         else
           CommandParameters := string.Empty;
 
-        case IndexStr(Command, ['start']) of
+        case IndexStr(Command, ['start', 'getsize']) of
           0:
             begin
               Self.StartProcess;
+            end;
+          1:
+            begin
+              writeln(GetDirSize('Data'));
             end;
           -1:
             WriteLn('Command not found.');
@@ -76,22 +87,76 @@ procedure TMainCore.StartProcess;
 var
   WordFile: TextFile;
   Worded: String;
+  WordString: TStringList;
+  Count: Int8;
 begin
-  // Map file to variable
-  AssignFile(WordFile, WordFileName);
+  WordString := TStringList.Create;
+  try
+    // Map file to variable
+    AssignFile(WordFile, WordFileName);
 
-  // Open the file handle for read/write
-  Reset(WordFile);
+    // Open the file handle for read/write
+    Reset(WordFile);
 
-  // loop for read by each line
-  while not Eof(WordFile) do
-  begin
-    ReadLn(WordFile, Worded);
-    //WriteLn(Worded);
+    // loop for read by each line
+    while not Eof(WordFile) do
+    begin
+      ReadLn(WordFile, Worded);
+      // we must ensure that name has more than 2 lengths
+      if Length(Worded) >= 2 then
+      begin
+        // Clear previous string first
+        WordString.Clear;
+        for Count := 0 to 99 do
+        begin
+          WordString.Add(Worded);
+        end;
+
+        // check if level 1 folder is existed if not create one
+        if not DirectoryExists(Format('%s/%s', [FolderName, Worded[1]])) then
+          CreateDir(Format('%s/%s', [FolderName, Worded[1]]));
+
+        // check if level 2 folder is existed if not create one
+        if not DirectoryExists(Format('%s/%s/%s', [FolderName, Worded[1], Worded[2]])) then
+          CreateDir(Format('%s/%s/%s', [FolderName, Worded[1], Worded[2]]));
+
+        // save to file
+        WordString.SaveToFile(Format('%s/%s/%s/%s.txt', [FolderName, Worded[1], Worded[2], LowerCase(Worded)]));
+      end;
+    end;
+
+    // close the file handle
+    CloseFile(WordFile);
+  finally
+    WordString.Free;
   end;
+end;
 
-  // close the file handle
-  CloseFile(WordFile);
+function TMainCore.GetDirSize(const Path: String): UInt32;
+var
+  TotalSize: LongInt;
+  procedure Find(Str: String);
+  var
+    MySearch: TSearchRec;
+    FindResult: Integer;
+  begin
+    FindResult := FindFirst(Str + '\*.*', faArchive + faHidden + faAnyFile +
+      faVolumeID + faSysFile + faDirectory, MySearch);
+    while FindResult = 0 do
+    begin
+      if (MySearch.Attr = faDirectory) and (MySearch.Name <> '.') and
+        (MySearch.Name <> '..') then
+        Find(Str + '\' + MySearch.Name)
+      else
+        TotalSize := TotalSize + MySearch.Size;
+      FindResult := FindNext(MySearch);
+    end;
+    FindClose(MySearch);
+  end;
+begin
+  TotalSize := 0;
+  Find(Path);
+  Result := TotalSize;
 end;
 
 end.
