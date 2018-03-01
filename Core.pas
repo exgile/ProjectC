0aa1b8aaebdc462380bb465719ae33f292da7cd6
topkung1 <hyperzaster@gmail.com>
2018-03-01 16:45:35 +0700

    add command-line function
    users can control application by pressing the commands

--- Core.pas
+++ Core.pas
@@ -2,9 +2,14 @@ unit Core;
 
 interface
 
+uses
+  System.Threading, System.SysUtils, System.StrUtils, XSuperObject;
+
 type
   TMainCore = class
     private
+      fCommand: ITask;
+      procedure StartCommand;
     public
       constructor Create;
       destructor Destroy;
@@ -16,12 +21,52 @@ implementation
 
 constructor TMainCore.Create;
 begin
-
+  // Start the command-line
+  Self.StartCommand;
 end;
 
 destructor TMainCore.Destroy;
 begin
 
+end;
+
+procedure TMainCore.StartCommand;
+var
+  Command, CommandParameters: string;
+  InputArray: TArray<string>;
+  C: string;
+  D: Double;
+begin
+  fCommand := TTask.Create(
+    procedure()
+    begin
+      while True do
+      begin
+        Readln(C);
+        InputArray := C.Split([' ']);
+
+        if Length(InputArray) >= 1 then
+          Command := InputArray[0]
+        else
+          Command := string.Empty;
+
+        if Length(InputArray) >= 2 then
+          CommandParameters := InputArray[1]
+        else
+          CommandParameters := string.Empty;
+
+        case IndexStr(Command, ['start']) of
+          0:
+            begin
+              WriteLn('Begin to started');
+            end;
+          -1:
+            WriteLn('Command not found.');
+        end;
+      end;
+    end);
+
+  fCommand.Start;
 end;
 
 end.
